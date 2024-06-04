0376b3cdb2274ebe5d1bbcb8f5f89ba43f31c930
MRubacha5 <mrubacha@itba.edu.ar>
2024-06-04 14:55:59 -0300

    Constants.h update

--- constants.h
+++ constants.h
@@ -1,6 +1,9 @@
 //Hardcoded values for columns and rows. Code should work if these are modified.
 #define ROW 10	
 #define COL 10
+//display
+#define DISPLAY_X 1000
+#define DISPLAY_Y 1000
 //All special case or error codes are negative
 #define ERR_INPUT (-1)
 #define END_PROG (-2)
